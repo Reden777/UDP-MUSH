a2990682b56109a76cdc5353db628b15deff748f
Reden <arturoandressc@gmail.com>
2026-09-06 14:04:23 -0300

    Removed & moved clutter

--- OtherIdeas/win.c
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	MessageBox(NULL, "Hello from the Win32 API!", "Msys2 gcc test", MB_OK | MB_ICONINFORMATION);
-	return 0;
-}
